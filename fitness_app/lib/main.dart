ed3cfe647b4ea315f342440e87cd79fd1dfddc4b
Victor A <victor.avalos01@utrgv.edu>
2023-01-23 15:59:13 -0600

    Initial Commit

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fitness App Demo',
+      title: 'SP Fitness App Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
