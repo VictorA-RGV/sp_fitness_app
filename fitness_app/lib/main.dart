2246a49258357421b12f1e6ff852351308d60d1f
Victor A <victor.avalos01@utrgv.edu>
2023-01-23 15:55:51 -0600

    Initial Commit change title

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// test VA
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fitness App Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
