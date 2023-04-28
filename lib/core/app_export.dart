320cc88b300516436ed5ee17663edaa82852bb42
garzaadrian <77303355+garzaadrian@users.noreply.github.com>
2023-04-27 22:38:43 -0500

    addedmaster

--- lib/core/app_export.dart
+++ lib/core/app_export.dart
@@ -1,6 +1,4 @@
 
 
-//export 'package:sp_fitness_app/core/utils/image_constant.dart';
 export 'package:sp_fitness_app/core/utils/color_constant.dart';
 export 'package:sp_fitness_app/core/utils/math_utils.dart';
-//export 'package:sp_fitness_app/widgets/common_image_view.dart';
--- lib/screens/Achivements/Stats.dart
+++ lib/screens/Achivements/Stats.dart
@@ -3,9 +3,8 @@ import 'package:sp_fitness_app/core/app_export.dart';
 import 'package:sp_fitness_app/screens/home/home.dart';
 import 'package:sp_fitness_app/shared/widgets/custom_button.dart';
 import 'package:sp_fitness_app/shared/widgets/spacing.dart';
-//import 'package:sp_fitness_app/shared/widgets/common_image_view.dart';
 import 'package:flutter/material.dart';
-//import 'package:easy_localization/easy_localization.dart';
+
 
 class SummaryScreen extends StatelessWidget {
   @override
